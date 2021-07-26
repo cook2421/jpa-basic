a21f9d45e655d25b00b286d050d48e9415131059
anylogic_국승현 <cooksh2421>
2021-07-26 10:06:04 +0900

    fixed untracked files

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,14 +14,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            // 비영속 상태
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-
-            // 영속 상태
-            em.persist(member);
 
             tx.commit();
 
